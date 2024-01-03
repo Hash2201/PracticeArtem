f207f80c143f950c86777491436ce9902e73c30e
Hash2201 <testingbuyer00@gmail.com>
2024-01-03 17:28:11 +0500

    second commit

--- cypress/e2e/testfirst.cy.js
+++ cypress/e2e/testfirst.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://qaseller.cybermart.pk/')
+/// <reference types="cypress"/>
+describe('Our First test suite', () => {
+  it('visit url', () => {
+
+    cy.visit('/')
+    cy.contains(/forms/i).click() // case insensitivity
+    cy.contains('Form Layouts').click()
+    cy.get('input')
+    cy.get('#inputEmail1')
+    cy.get('[id="inputEmail1"]')
+    cy.get('input[id="inputEmail1"]')
+
+
+
+
   })
 })

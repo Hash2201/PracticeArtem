cdd1e9b96fb12e4d90726a1faec140bc9d25b5a4
Hash2201 <testingbuyer00@gmail.com>
2024-01-09 18:32:00 +0500

    9thedec

--- cypress/e2e/testfirst.cy.js
+++ cypress/e2e/testfirst.cy.js
@@ -22,7 +22,7 @@ describe("Our First test suite", () => {
     cy.contains("nb-card", "Horizontal form").find('[type="email"]');
   });
 
-  it.only("Second test", () => {
+  it("Second test", () => {
     cy.visit("/");
     cy.contains(/forms/i).click(); // case insensitivity
     cy.contains("Form Layouts").click();
@@ -30,4 +30,38 @@ describe("Our First test suite", () => {
     cy.contains("nb-card", "using the grid").find('[for="inputEmail1"]').should('contain','email')
   
   });
+
+  it.only("invoke command ", () => {
+    cy.visit("/");
+    cy.contains(/forms/i).click(); // case insensitivity
+    cy.contains("Form Layouts").click();
+
+    cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address');
+    // assertion
+
+    //assertion in jquery style 
+    cy.get('[for="exampleInputEmail1"]').then(label => {
+      expect(label.text()).to.equal('Email address')
+
+    })
+
+    //wrap method 
+    cy.get('[for="exampleInputEmail1"]').invoke('text').then(text => {
+      expect(text).to.equal('Email address')
+    })
+
+    cy.contains('nb-card','Basic form')
+                  .find('nb-checkbox')
+                  .click()
+                  .find('.custom-checkbox')
+                  .invoke('attr','class')
+                  .should('contain', 'checked')
+
+
+  });
+
+  
+
+
+
 });
